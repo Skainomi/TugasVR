e3b507b2f00406624459a4c58c809dd15d0b9f65
Skainomi <maaid.chan@gmail.com>
2021-12-15 20:15:36 +0700

    initial commit

---
+++ Assets/Script/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float playerSpeed = 12f;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Fire1")){
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+    }
+}
